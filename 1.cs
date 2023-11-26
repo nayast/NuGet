a3b9ea81aa93f4378ef8b0981a9f58dc01ad15d6
nayast <120473259+nayast@users.noreply.github.com>
2023-11-27 04:53:26 +0700

    Update 1.cs

--- 1.cs
+++ 1.cs
@@ -1,1 +1,19 @@
-//helloh
+фамилии -> номер
+insert into ClientOrder (idClient)
+select Client.idClient, ClientOrder.Surname
+from ClientOrder, Client
+where ClientOrder.idClient = Client.Surname
+
+grid adapter
+DataSet ds = new DataSet();
+using(...){
+ conn.Open();
+ sql = ...;
+ SqlDataAdapter ada = new SqlDataAdapter(sql,conn);
+ ada.Fill(ds);
+}
+like %.%
+FullRowSelect
+from-where-group by-order by- desc
+заполнение панели: layout.Controls.Clear(); 
+foreach (var .. in GetList()) Control item = new Control(class); layout.Controls.Add(item);
